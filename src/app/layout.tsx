3051118c6c6c07bd7e628f13a3f32f9be499ffd2
GreyMullet <ogonannikita52@gmail.com>
2026-08-25 18:01:54 +0300

    phone pics changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,8 +62,8 @@ export const metadata: Metadata={
         },
     },
     icons: {
-        icon: '/favicon.ico',
-        apple: '/apple-touch-icon.png',
+        icon: '/icon-192.png',
+        apple: '/icon-192.png',
     },
 }
 
